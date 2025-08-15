3e1fdbaa98e6c11832d817aee102c119e58dc87c
jjdev86 <vinmajj@gmail.com>
2025-08-15 16:03:54 -0700

    add cookie-parser and use cors for frontend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ const notificationsRoutes = require('./routes/notifications');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev')); // Logging middleware
