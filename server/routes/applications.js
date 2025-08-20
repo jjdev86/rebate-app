a70b2e941066ba2b29e6885b181a52e39f1005dc
jjdev86 <vinmajj@gmail.com>
2025-08-19 22:55:31 -0700

    fix import createApplicationValidation location

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
 const appController = require('../controllers/applicationController');
-const { createApplicationValidation } = require('../middleware/validators/applicationValidation');
+const { createApplicationValidation } = require('../validators/applicationValidation');
 
 router.use(auth);
 // Apply authentication middleware to all routes in this file
