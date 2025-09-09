093c30a031ce72044408912fb018398ee4c53286
jjdev86 <vinmajj@gmail.com>
2025-09-09 15:22:31 -0700

    create product_eligibilities model

---
+++ server/migrations/20250907-create-product-eligibility.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('product_eligibilities', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      productId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      startDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      endDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('product_eligibilities');
+  },
+};
